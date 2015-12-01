4c687a8f197b895da66602289f0e5ed48a767381
hudongyang <hudongyang@benmu-health.com>
2015-12-01 22:31:21 +0800

    tab->space

--- src/subView.js
+++ src/subView.js
@@ -1,64 +1,64 @@
 let Backbone = require('backbone'),
 
-	_ = require('underscore'),
+    _ = require('underscore'),
 
-	User = Backbone.Model.extend({}),
-	Users = Backbone.Collection.extend({
-		model: User
-	}),
+    User = Backbone.Model.extend({}),
+    Users = Backbone.Collection.extend({
+        model: User
+    }),
 
-	users = new Users([{
-		id: 1,
-		name: 'hudy'
-	}, {
-		id: 2,
-		name: 'caih'
-	}, {
-		id: 3,
-		name: 'huyf'
-	}]),
+    users = new Users([{
+        id: 1,
+        name: 'hudy'
+    }, {
+        id: 2,
+        name: 'caih'
+    }, {
+        id: 3,
+        name: 'huyf'
+    }]),
 
-	UserItemView = Backbone.View.extend({
-		tagName: 'li',
-		template: '<%= name %>',
-		events: {
-			'click': 'showName'
-		},
-		render() {
-			let html = _.template(this.template)(this.model.toJSON());
+    UserItemView = Backbone.View.extend({
+        tagName: 'li',
+        template: '<%= name %>',
+        events: {
+            'click': 'showName'
+        },
+        render() {
+            let html = _.template(this.template)(this.model.toJSON());
 
-			this.$el.html(html);
+            this.$el.html(html);
 
-			return this;
-		},
-		showName: function() {
-			console.log(this.model.get('name'));
-		}
-	}),
+            return this;
+        },
+        showName: function() {
+            console.log(this.model.get('name'));
+        }
+    }),
 
-	UsersView = Backbone.View.extend({
-		tagName: 'ul',
-		render() {
-			let fragment = document.createDocumentFragment(),
-				itemView;
+    UsersView = Backbone.View.extend({
+        tagName: 'ul',
+        render() {
+            let fragment = document.createDocumentFragment(),
+                itemView;
 
-			this.collection.each(function(model) {
-				itemView = new UserItemView({
-					model: model
-				});
+            this.collection.each(function(model) {
+                itemView = new UserItemView({
+                    model: model
+                });
 
-				fragment.appendChild(itemView.render().el);
-			}, this);
+                fragment.appendChild(itemView.render().el);
+            }, this);
 
-			this.$el.html(fragment);
+            this.$el.html(fragment);
 
-			return this;
-		}
-	}),
+            return this;
+        }
+    }),
 
-	usersView = new UsersView({
-		collection: users
-	}).render();
+    usersView = new UsersView({
+        collection: users
+    }).render();
 
 
 $('body').prepend(usersView.el);
